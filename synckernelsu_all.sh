052c3c284e5a06c56a62e7f88c34c4a4fb8730b5
Coconut <coconutat@protonmail.com>
2024-08-01 20:06:26 +0800

    KSU Script: Fix error

--- synckernelsu_all.sh
+++ synckernelsu_all.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
-if [ -d KernelSU ];
-then
-    echo "Find KernelSU Floder,will remove it"
-	rm -rf KernelSU
+
+if [ -d KernelSU ]; then
+    echo "Found KernelSU Folder, removing it..."
+    rm -rf KernelSU
+	rm -rf drivers/kernelsu
 else
-	echo "KernelSU Floder,good."
+    echo "KernelSU Folder not found, proceeding..."
 fi
 
+read -p "Please enter the version number (A/a is tags version.B/b is main version.C/c is special v0.9.2 Version For Huawei): " version
 
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+if [ "$version" == "" ]; then
+    echo "No version specified. Exiting."
+    exit 1
+fi
+
+case $version in
+    [Aa]*)
+        read -p "Please enter the version number you want to install: " specified_version
+        curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v"$specified_version"
+        ;;
+    [Bb]*)
+        curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+        ;;
+    [Cc]*)
+        curl -LSs https://raw.githubusercontent.com/Coconutat/KernelSU_backup/0.9.2_upstream_backup/kernel/setup.sh | bash -s 0.9.2_upstream_backup
+        ;;
+    *)
+        echo "Invalid option. Exiting."
+        exit 1
+        ;;
+esac
